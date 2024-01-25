31721a6b49bab41a8066c0c7bd4b43cf1dcc7a85
Emil Widlund <hello@emilwidlund.com>
2024-01-25 08:29:24 +0100

    fix control

--- apps/web/src/components/Control/Control.tsx
+++ apps/web/src/components/Control/Control.tsx
@@ -1,6 +1,6 @@
 import { Input, Output } from '@bitspace/circuit';
 import { observer } from 'mobx-react-lite';
-import { ChangeEventHandler, useCallback, useEffect, useMemo, useState } from 'react';
+import { ChangeEventHandler, KeyboardEventHandler, useCallback, useEffect, useMemo, useState } from 'react';
 
 export interface ControlProps {
     port: Input | Output;
@@ -20,30 +20,29 @@ export const Control = observer(({ port }: ControlProps) => {
         };
     }, [port]);
 
+    const handleKeydown: KeyboardEventHandler<HTMLInputElement> = useCallback(e => {
+        e.stopPropagation();
+    }, []);
+
     const handleChange: ChangeEventHandler<HTMLInputElement> = useCallback(
         e => {
-            e.preventDefault();
-            e.stopPropagation();
-
             if (port.type.name === 'Number') {
-                setValue(parseFloat(e.target.value));
+                port.next(port.type.validator.parse(parseFloat(e.target.value)));
             }
         },
         [port]
     );
 
-    const handleBlur = useCallback(() => {
-        port.next(value);
-    }, [value]);
-
     return (
         <input
             className="px-2 py-1 border rounded-md border-slate-200 w-full"
+            type="number"
+            step={0.01}
             placeholder={port.type.name}
+            onKeyDown={handleKeydown}
             value={value}
             disabled={disabled}
             onChange={handleChange}
-            onBlur={handleBlur}
         />
     );
 });
