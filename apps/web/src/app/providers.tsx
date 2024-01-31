01182ee6d55a753cd0f59e882eec6250b7cd1a5a
Emil Widlund <hello@emilwidlund.com>
2024-01-31 19:53:56 +0100

    fix posthog

--- apps/web/src/app/providers.tsx
+++ apps/web/src/app/providers.tsx
@@ -3,6 +3,14 @@
 import { PropsWithChildren } from 'react';
 import { HotkeysProvider } from 'react-hotkeys-hook';
 import { SessionProvider } from 'next-auth/react';
+import posthog from 'posthog-js';
+
+if (typeof window !== 'undefined') {
+    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+        api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+        capture_pageview: false // Disable automatic pageview capture, as we capture manually
+    });
+}
 
 export const Providers = ({ children }: PropsWithChildren) => {
     return (
