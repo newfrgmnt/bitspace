11b8b584c5f13ebeb63461328cfbc4d9f5f6fe25
Emil Widlund <hello@emilwidlund.com>
2024-02-04 15:41:47 +0100

    remove logging

--- apps/web/src/server/mutations/createConnection.ts
+++ apps/web/src/server/mutations/createConnection.ts
@@ -5,8 +5,6 @@ import { PrismaClient } from '@prisma/client';
 export const createConnection = async (fromId: string, toId: string) => {
     const prisma = new PrismaClient();
 
-    console.log(fromId, toId);
-
     return await prisma.connection.create({
         data: {
             fromId,
--- apps/web/src/server/mutations/moveNode.ts
+++ apps/web/src/server/mutations/moveNode.ts
@@ -5,8 +5,6 @@ import { PrismaClient } from '@prisma/client';
 export const moveNode = async (nodeId: string, position: { x: number; y: number }) => {
     const prisma = new PrismaClient();
 
-    console.log(nodeId, position);
-
     return await prisma.nodePosition.update({
         where: { nodeId },
         data: {
