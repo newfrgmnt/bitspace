91f7cfd9620b9f594952d68fe3753ca29cbf336e
Emil Widlund <hello@emilwidlund.com>
2024-06-02 15:06:38 +0200

    use polar sdk

--- apps/web/src/app/(landing)/createSubscription.ts
+++ apps/web/src/app/(landing)/createSubscription.ts
@@ -2,20 +2,16 @@
 
 import { env } from '@/env';
 import { getServerSideAPI } from '../../server/utils';
-
-const api = getServerSideAPI();
+import { Platforms } from '@polar-sh/sdk';
 
 export const createSubscription = async (email: string) => {
-    return fetch(
-        'https://api.polar.sh/api/v1/subscriptions/subscriptions/email?organization_name=newfrgmnt&platform=github',
-        {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                Accept: 'application/json',
-                Authorization: `Bearer ${env.POLAR_ACCESS_KEY}`
-            },
-            body: JSON.stringify({ email })
+    const polar = getServerSideAPI(env.POLAR_ACCESS_KEY);
+
+    return polar.subscriptions.createEmailSubscription({
+        organizationName: 'newfrgmnt',
+        platform: Platforms.GITHUB,
+        subscriptionCreateEmail: {
+            email
         }
-    ).then(res => res.json());
+    });
 };
