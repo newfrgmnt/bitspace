c9673fac88768377ee4c468594d7af16e0b07bd5
Emil Widlund <hello@emilwidlund.com>
2024-12-28 23:27:12 +0100

    adjust env

--- apps/web/src/env.ts
+++ apps/web/src/env.ts
@@ -9,9 +9,7 @@ export const env = createEnv({
         NEXT_PUBLIC_POSTHOG_API_KEY: z.string()
     },
     server: {
-        POLAR_ACCESS_KEY: z.string(),
         OPENAI_API_KEY: z.string(),
-        VERCEL_URL: z.string(),
         BLOB_READ_WRITE_TOKEN: z.string(),
         DATABASE_URL: z.string(),
         DIRECT_URL: z.string()
@@ -22,9 +20,7 @@ export const env = createEnv({
             process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
         NEXT_PUBLIC_POSTHOG_API_KEY: process.env.NEXT_PUBLIC_POSTHOG_API_KEY,
         NEXT_PUBLIC_POSTHOG_HOST: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-        POLAR_ACCESS_KEY: process.env.POLAR_ACCESS_KEY,
         OPENAI_API_KEY: process.env.OPENAI_API_KEY,
-        VERCEL_URL: process.env.VERCEL_URL,
         BLOB_READ_WRITE_TOKEN: process.env.BLOB_READ_WRITE_TOKEN,
         DATABASE_URL: process.env.DATABASE_URL,
         DIRECT_URL: process.env.DIRECT_URL
